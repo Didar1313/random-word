a8788204ad607584939f4d598892025a065fe267
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-08-28 23:56:47 +0600

    add Meaning model to represent dictionary meanings

---
+++ src/main/java/com/example/randomword/model/Meaning.java
@@ -0,0 +1,24 @@
+package com.example.randomword.model;
+
+import java.util.List;
+
+public class Meaning {
+    private String partOfSpeech;
+    private List<DefinitionWithPOS> definitionWithPOS;
+
+    public String getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public void setPartOfSpeech(String partOfSpeech) {
+        this.partOfSpeech = partOfSpeech;
+    }
+
+    public List<DefinitionWithPOS> getDefinitions() {
+        return definitionWithPOS;
+    }
+
+    public void setDefinitions(List<DefinitionWithPOS> definitionWithPOS) {
+        this.definitionWithPOS = definitionWithPOS;
+    }
+}
