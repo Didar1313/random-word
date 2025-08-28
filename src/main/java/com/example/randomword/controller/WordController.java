c3559c13a0d142d53b4d7abd83d789298e2252d2
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-08-29 01:54:14 +0600

    swagger documentation.

--- src/main/java/com/example/randomword/controller/WordController.java
+++ src/main/java/com/example/randomword/controller/WordController.java
@@ -2,6 +2,7 @@ package com.example.randomword.controller;
 
 import com.example.randomword.model.word.WordResponse;
 import com.example.randomword.service.WordService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class WordController {
 
     private final WordService wordService;
 
+    @Operation(summary = "Get the word")
     @GetMapping("/wordOfTheDay")
     public WordResponse getWordOfTheDay() {
         return wordService.getWordOfTheDay();
